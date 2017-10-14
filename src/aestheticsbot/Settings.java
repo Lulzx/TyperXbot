867e847b795a24973ab7c97048543b97ac65f57f
Lulzx <wiserishabh@gmail.com>
2017-10-14 11:30:20 +0530

    Lol

--- src/aestheticsbot/Settings.java
+++ src/aestheticsbot/Settings.java
@@ -5,7 +5,7 @@ package aestheticsbot;
  * @author Luke
  */
 public class Settings {
-    public static final String BOT_TOKEN = "";
-    public static final String BOT_USERNAME = "typerobot";
+    public static final String BOT_TOKEN = "473713776:AAHuUoRAR9-WKzwfN1mtYwLe4ZL1mBUwf0Q";
+    public static final String BOT_USERNAME = "TypxBot";
     public static final Long BOT_MASTER = 0L;
 }
